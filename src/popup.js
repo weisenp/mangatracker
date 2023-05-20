3f62d820c8c1985f2c3949c360d48e54c1054a83
Weisen Peng <68366979+weisenp@users.noreply.github.com>
2023-05-21 00:06:09 +1000

    prettier formated

--- src/popup.js
+++ src/popup.js
@@ -1,17 +1,17 @@
-const pb = new PocketBase('https://pocketbase.libus.dev/');
+const pb = new PocketBase("https://pocketbase.libus.dev/");
 
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-    let url = tabs[0].url;
-    if (!url.includes("https://www.asurascans.com/manga/")) {
-        // hides the div with class of menu and shows the div with class of unavailable
-        document.querySelector("body > div.menu").style.display = "none";
-        return document.querySelector("body > div.unavailable").style.display = "flex";
-    }
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+  let url = tabs[0].url;
+  if (!url.includes("https://www.asurascans.com/manga/")) {
+    // hides the div with class of menu and shows the div with class of unavailable
+    document.querySelector("body > div.menu").style.display = "none";
+    return (document.querySelector("body > div.unavailable").style.display =
+      "flex");
+  }
 
-    //if not logged in show login screen
-    if (!pb.authStore.isValid) {
-        document.querySelector("body > div.login").style.display = "block"
-        document.querySelector("body > div.menu").style.display = "none";
-    }
+  //if not logged in show login screen
+  if (!pb.authStore.isValid) {
+    document.querySelector("body > div.login").style.display = "block";
+    document.querySelector("body > div.menu").style.display = "none";
+  }
 });
-
--- src/scripts/save.js
+++ src/scripts/save.js
@@ -1,42 +1,39 @@
 (async () => {
-    const src = chrome.runtime.getURL("src/dist/pocketbase.js");
-    const contentMain = await import(src);
+  const src = chrome.runtime.getURL("src/dist/pocketbase.js");
+  const contentMain = await import(src);
 
-    const pb = new PocketBase('https://pocketbase.libus.dev/');
+  const pb = new PocketBase("https://pocketbase.libus.dev/");
 
-    // if we are on asura scans
-    if (window.location.host == "www.asurascans.com") {
-        // get the manhwahs filtering by the name
-        const resultList = await pb.collection('manhwas').getList(1, 1, {
-            filter: `name = "${document.title}"`,
-        });
+  // if we are on asura scans
+  if (window.location.host == "www.asurascans.com") {
+    // get the manhwahs filtering by the name
+    const resultList = await pb.collection("manhwas").getList(1, 1, {
+      filter: `name = "${document.title}"`,
+    });
 
-        console.log(resultList);
-        // if the manhwa is already saved
-        if (resultList.totalItems > 0) {
-            // check to see if we should update the manwha 
-            if (new Date(resultList.items[0].updatableAt) <= Date() || true) {
+    console.log(resultList);
+    // if the manhwa is already saved
+    if (resultList.totalItems > 0) {
+      // check to see if we should update the manwha
+      if (new Date(resultList.items[0].updatableAt) <= Date() || true) {
+        let updatableAt = new Date();
+        updatableAt.setDate(updatableAt.getDate() + 2);
+        // document.querySelector("div.infox > div:nth-child(7) > div:nth-child(2) > span > time")
 
-                let updatableAt = new Date()
-                updatableAt.setDate(updatableAt.getDate() + 2);
-                // document.querySelector("div.infox > div:nth-child(7) > div:nth-child(2) > span > time")
+        let lastUpdated = new Date();
+        // const data = {
+        //     "latestChapter": "test",
+        //     "lastUpdated": ,
+        //     updatableAt
+        // };
 
-                let lastUpdated = new Date()
-                // const data = {
-                //     "latestChapter": "test",
-                //     "lastUpdated": ,
-                //     updatableAt
-                // };
-
-                // const record = await pb.collection('manhwas').update(resultList.items[0].id, data);
-            }
-        }
-        let element = document.querySelector("div.infox > div.flex-wrap > div.fmed:nth-child(2) > span > time")
-        element.innerHTML = "hello"
+        // const record = await pb.collection('manhwas').update(resultList.items[0].id, data);
+      }
     }
+    let element = document.querySelector(
+      "div.infox > div.flex-wrap > div.fmed:nth-child(2) > span > time"
+    );
+    element.innerHTML = "hello";
+    console.log(element);
+  }
 })();
-
-
-
-
-
